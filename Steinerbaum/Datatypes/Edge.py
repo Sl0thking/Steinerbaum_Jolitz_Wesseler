b12783cdfa3ebbf2f30931c372f43f6963775d7e
Sl0thking <k.jolitz-dev@outlook.de>
2016-10-18 09:19:29 +0200

    #Init
    ...

---
+++ Steinerbaum/Datatypes/Edge.py
@@ -0,0 +1,17 @@
+'''
+Created on 18.10.2016
+
+@author: Slothking
+'''
+
+class Edge(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
---
+++ Steinerbaum/Datatypes/Graph.py
@@ -0,0 +1,37 @@
+'''
+Created on 18.10.2016
+
+@author: Slothking
+'''
+
+class Graph(object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        __nodeMap = {}
+        
+    
+    def addNode(self, node_id, isTerminal): 
+        pass
+    
+    def addEdge(self, start_node_id, end_node_id, value):
+        pass
+    
+    def modifyEdgeValue(self, start_node_id, end_node_id, value):
+        pass
+    
+    def delNode(self, node_id):
+        pass
+    
+    def delEdge(self, start_node_id, end_node_id):
+        pass
+    
+    def getEdges(self, node_id):
+        pass
+    
+    
