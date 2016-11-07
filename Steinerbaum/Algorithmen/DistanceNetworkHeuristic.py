c9bfec4740018de82d10674aadf85f5ec49a9505
Sl0thking <k.jolitz-dev@outlook.de>
2016-11-07 16:02:47 +0100

    #Last fix

--- Steinerbaum/Algorithmen/DistanceNetworkHeuristic.py
+++ Steinerbaum/Algorithmen/DistanceNetworkHeuristic.py
@@ -18,17 +18,39 @@ if __name__ == '__main__':
     print "#                Source graph              #"
     print "############################################"
     print graph
-    wait = raw_input()
+    wait = raw_input("...")
     shortest_paths = getShortestPaths(graph)
     distance_graph = getDistanceGraph(graph, shortest_paths)
     print "############################################"
-    print "#               Distance Graph             #"
+    print "#     1. Calculated Distance Graph         #"
     print "############################################"
     print distance_graph
-    wait = raw_input()
+    wait = raw_input("...")
     min_distance_graph = getMinimalSpanningtree(distance_graph)
+    print "############################################"
+    print "#      2. minimal Distance Graph           #"
+    print "############################################"
+    print min_distance_graph
+    wait = raw_input("...")
     transform_graph = transformDistanceToSteinerbaum(min_distance_graph, shortest_paths)
     print "############################################"
-    print "#                Steinerbaum               #"
+    print "#        3. transformed Graph             #"
     print "############################################"
     print transform_graph
+    wait = raw_input("...")
+    min_transform_graph = getMinimalSpanningtree(transform_graph)
+    print "############################################"
+    print "#      4. minimal transformed Graph        #"
+    print "############################################"
+    print min_transform_graph
+    wait = raw_input("...")
+    nodes = min_transform_graph.getNodes()
+    
+    for node in nodes:
+        edges = min_transform_graph.getEdgesOfNode(node.getID())
+        if len(edges) < 2 and not node.isTerminal():
+            min_transform_graph.delNode(node.getID())
+    print "############################################"
+    print "#             5. Steinerbaum               #"
+    print "############################################"
+    print min_transform_graph
